6f29911cd5bc3fa02abd03cff09ebcb77c9a7d51
erkron <khiglix@gmail.com>
2025-08-12 16:08:32 +0300

    Go Init

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"goji.io"
+	"goji.io/pat"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Travler!")
+}
+
+func main() {
+	mux := goji.NewMux()
+	mux.HandleFunc(pat.Get("/"), hello)
+
+	http.ListenAndServe("localhost:8000", mux)
+	fmt.Println("Server is running on port 8000")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module matcha
+
+go 1.22.2
+
+require goji.io v2.0.2+incompatible // indirect
